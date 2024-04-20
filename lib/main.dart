e91621df7516c47b96ce0faf7c530b414355ae16
Uriel González Núñez <gonzalez.nunez.uriel@gmail.com>
2024-04-19 21:57:17 -0500

    Success
    Not I can write the clear feature like this. Which is kind of neat
    ... but ...
    What happens if I need to insert this kind of buttons in the front
    or the row? What about in the middle? What about arbitrary button
    inserts?
    
    This pattern is good for now, but it does not scale.

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Column(
           children: [
             Text(_calculation),
-            buildRow(['7', '8', '9', 'clr']),
+            buildRow(['7', '8', '9'], [buildClearButton()]),
             buildRow(['4', '5', '6', '+']),
             buildRow(['1', '2', '3', '-']),
             buildRow(['0']),
